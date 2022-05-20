68a554e50b8be78aeba5c7595195d9ba26cfd092
JoelGhub <font.joel@gmail.com>
2022-05-20 19:35:46 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,56 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const icons = ["♦", "♥", "♠", "♣"];
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let array;
+  let carta;
+
+  //DOM//
+
+  /* Button */
+
+  let button = document.querySelector("#btn");
+  button.addEventListener("click", function() {
+    let elem = document.querySelector("#cards");
+    carta = icons[Math.floor(Math.random() * 4)];
+    array = numbers[Math.floor(Math.random() * 13)];
+    // console.log(array + carta);
+    // console.log(elem);
+    elem.innerHTML = `
+  <div class="inner-div">${carta}</div>
+  <div class="inner-div">${array} </div>
+  <div class="inner-div bottom">${carta}</div>
+  `;
+  });
+
+  /* set 10 secs to change */
+
+  setInterval(myTimer, 10000);
+
+  function myTimer() {
+    let elem = document.querySelector("#cards");
+    carta = icons[Math.floor(Math.random() * 4)];
+    array = numbers[Math.floor(Math.random() * 13)];
+    // console.log(array + carta);
+    // console.log(elem);
+    elem.innerHTML = `
+  <div class="inner-div">${carta}</div>
+  <div class="inner-div">${array} </div>
+  <div class="inner-div bottom">${carta}</div>
+  `;
+  }
+
+  /* set width and height */
+  let button2 = document.querySelector("#set");
+  button2.addEventListener("click", function() {
+    let innerdiv = document.querySelector(".card");
+    let width = document.querySelector(".width");
+    let height = document.querySelector(".height");
+    let show = width.value;
+    let show2 = height.value;
+
+    document.querySelector(".card").style.width = show + "px";
+    document.querySelector(".card").style.height = show2 + "px";
+  });
 };
